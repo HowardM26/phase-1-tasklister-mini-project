1266083f0f6102e46fff0ec84602730a1d1772a3
+ <+>
2023-06-14 22:04:43 -0400

    displays task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
+  addingEventListeners()
+})
   // your code here
-});
+  function addingEventListeners() {
+    document.getElementById("create-task-form").addEventListener("submit", handleFormSubmit)
+  }
+
+  function handleFormSubmit(e) {
+    e.preventDefault()
+    //console.log(e)
+     const task = e.target["new-task-description"].value
+     
+     displayTask(task)
+  }
+
+  function displayTask(task) {
+     const taskUl = document.getElementById("tasks")
+     
+     const taskLi = document.createElement("li")
+
+     taskLi.textContent = task
+     taskUl.appendChild(taskLi) 
+  }
